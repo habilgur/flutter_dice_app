3cfcfa6d61d43f7ef4589dd9fcb3296f89855a8c
Asiaminor 78 <asiaminor78@hotmail.com>
2019-12-04 14:51:29 +0300

    change

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,154 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.green[900],
-        body: DiceBody(),
-      ),
+      home: DiceBody(),
     );
   }
 }
 
+class DiceBody extends StatefulWidget {
+  @override
+  _DiceBodyState createState() => _DiceBodyState();
+}
+
+class _DiceBodyState extends State<DiceBody> {
+  int dice1num = 0;
+  int dice2num = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[900],
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Tavla",
+        ),
+        backgroundColor: Colors.green[900],
+      ),
+      body: buildDiceCanvas(),
+    );
+  }
+
+  Widget buildDiceCanvas() {
+    return SafeArea(
+      child: Center(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  FloatingActionButton(
+                    splashColor: Colors.red,
+                    child: RotatedBox(
+                      child: Text("Salla"),
+                      quarterTurns: 2,
+                    ),
+                    onPressed: () {
+                      rollDices();
+                    },
+                  ),
+                ],
+                //alignment: Alignment.topLeft,
+              ),
+            ),
+            Expanded(
+              child: GestureDetector(
+                onTap: () {
+                  rollDices();
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    /// Expanded parent must be Column Row or Flex
+                    /// If child size is too big and applied Expanded ,
+                    /// Every Expanded child contracts with its parent so it will fit inside the parent
+                    Expanded(
+                      child: buildImage(dice1num),
+
+                      /// With flex number, all area portions  will be shared by expand widgets
+                      /// ( flex 1 & flex 3 means %33-%66)
+                      flex: 1,
+                    ),
+                    Expanded(
+                      child: buildImage(dice2num),
+                      flex: 1,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  FloatingActionButton(
+                    splashColor: Colors.red,
+                    child: RotatedBox(
+                      child: Text("Salla"),
+                      quarterTurns: 2,
+                    ),
+                    onPressed: () {
+                      rollDices();
+                    },
+                  ),
+                ],
+                //alignment: Alignment.topLeft,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void rollDices() {
+    setState(() {
+      dice1num = Random().nextInt(5);
+      dice2num = Random().nextInt(5);
+    });
+  }
+
+  buildImage(int number) {
+    switch (number) {
+      case 0:
+
+        /// We can use Image.asset instead of Image(image:AssetImage())
+        return Image.asset("assets/Alea_1.png");
+        break;
+      case 1:
+        return Image.asset("assets/Alea_2.png");
+
+        break;
+      case 2:
+        return Image(
+          image: AssetImage("assets/Alea_3.png"),
+        );
+        break;
+      case 3:
+        return Image(
+          image: AssetImage("assets/Alea_4.png"),
+        );
+        break;
+      case 4:
+        return Image(
+          image: AssetImage("assets/Alea_5.png"),
+        );
+        break;
+      case 5:
+        return Image(image: AssetImage("assets/Alea_6.png"));
+        break;
+      default:
+        return Image(image: AssetImage("assets/Alea_6.png"));
+    }
+  }
+}
+
+//ALTERNATİF------
+/*
 class DiceBody extends StatefulWidget {
   @override
   _DiceBodyState createState() => _DiceBodyState();
@@ -42,6 +182,7 @@ class _DiceBodyState extends State<DiceBody> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+
               Container(
                 height: 150,
                 width: 150,
@@ -100,4 +241,8 @@ class _DiceBodyState extends State<DiceBody> {
         return DecorationImage(image: AssetImage("assets/Alea_6.png"));
     }
   }
+
+
 }
+
+*/
